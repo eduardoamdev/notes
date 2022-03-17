37987f0f7a722633d615594e99221391cd7e81bb
root <eduardoamdev@gmail.com>
2022-03-17 11:05:06 +0100

    More fixes

--- middlewares/tokenAuth.js
+++ middlewares/tokenAuth.js
@@ -1,26 +1,26 @@
 const jwt = require('jsonwebtoken');
 
 const tokenAuth = (req, res, next) => {
-	let token = req.headers.token;
+  let token = req.headers.token;
 
-	if (!token) {
-		return res.json({
-			success: false,
-			message: 'No token provided'
-		});
-	}
+  if (!token) {
+    return res.json({
+      success: false,
+      message: 'No token provided'
+    });
+  }
 
-	jwt.verify(token, process.env.SECRET_WORD, (error, decoded) => {
-		if (error) {
-			return res.json({
-				success: false,
-				message: 'Incorrect token authentication'
-			});
-		} else {
-			req.userId = decoded.id;
-			next();
-		}
-	});
+  jwt.verify(token, process.env.SECRET_WORD, (error, decoded) => {
+    if (error) {
+      return res.json({
+        success: false,
+        message: 'Incorrect token authentication'
+      });
+    } else {
+    req.userId = decoded.id;
+      next();
+    }
+  });
 };
 
 module.exports = tokenAuth;
