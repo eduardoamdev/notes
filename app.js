cdc458669fbbf5e7c042bd41ea2fec458a03d189
eduardoamdev <eduardoamdev@gmail.com>
2022-08-07 10:24:25 +0200

    appjs identatinon changed

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-const checkUserPassword = require('./middlewares/checkUserPassword');
-const tokenAuth = require('./middlewares/tokenAuth');
+const checkUserPassword = require("./middlewares/checkUserPassword");
+const tokenAuth = require("./middlewares/tokenAuth");
 
-const authRoutes = require('./api/authRoutes');
-const userRoutes = require('./api/userRoutes');
+const authRoutes = require("./api/authRoutes");
+const userRoutes = require("./api/userRoutes");
 
-require('./dbConfig');
+require("./dbConfig");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/auth', checkUserPassword, authRoutes);
-app.use('/api/user', tokenAuth, userRoutes);
+app.use("/api/auth", checkUserPassword, authRoutes);
+app.use("/api/user", tokenAuth, userRoutes);
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server listening on port ${process.env.PORT}`);
+  console.log(`Server listening on port ${process.env.PORT}`);
 });
