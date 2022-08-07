c6b6f07cedf867df43df850fa8999bc016de22ac
eduardoamdev <eduardoamdev@gmail.com>
2022-08-07 11:16:43 +0200

    more changes

--- notes-client/src/components/Error.jsx
+++ notes-client/src/components/Error.jsx
@@ -3,15 +3,15 @@ import { useParams } from "react-router-dom";
 
 const Error = () => {
   let params = useParams();
-	let error = params.error;
-	return (
-		<div>
-			<NavBar private={false} />
-			<div className="main-container flex-center title-container">
+  let error = params.error;
+  return (
+    <div>
+      <NavBar private={false} />
+      <div className="main-container flex-center title-container">
         <h3>{error}</h3>
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 };
 
 export default Error;
